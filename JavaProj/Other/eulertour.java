1b2602c6b5c4ee819aeb4b9a189fd0c733cfc8a9
Raymond <20248577+javaarchive@users.noreply.github.com>
2018-12-23 20:32:41 -0800

    Create eulertour.java

---
+++ JavaProj/Other/eulertour.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class eulertour {
+	Stack<node> path = new Stack<node>();
+	LinkedList<node> full_path = new LinkedList<node>();
+	public void tour(node start, VisitListener vl) {
+		path.clear();
+		full_path.clear();
+		
+	}
+	public static void main(String[] args) {
+		
+	}
+
+}
